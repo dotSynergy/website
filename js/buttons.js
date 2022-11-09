194ee601872722e0b6c4779292de3c86b6e489b1
Samuele <dotsynergy@protonmail.com>
2022-11-09 17:21:48 +0100

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -6,6 +6,8 @@ let bioClose;
 let contClose;
 let portClose;
 
+let catchMovement;
+
 function limitScroll(e,container){
     
     var scroll = e.srcElement.scrollTop;
@@ -17,33 +19,35 @@ function limitScroll(e,container){
 }
 
 document.onreadystatechange = () => {
-    
-    bioContainer = document.getElementById("bio_container");
-    contContainer = document.getElementById("contacts_container");
-    portContainer = document.getElementById("portfolio_container");
-    
-    bioClose = document.getElementById("c_btn_b");
-    contClose = document.getElementById("c_btn_c");
-    portClose = document.getElementById("c_btn_p");
-    
-    bioContainer.addEventListener('scroll', function(e) {limitScroll(e, bioClose);});
-    
-    contContainer.addEventListener('scroll', function(e) {limitScroll(e, contClose);});
-    
-    portContainer.addEventListener('scroll', function(e) {limitScroll(e, portClose);});
+
+    if(document.readyState == 'complete'){
+        
+        bioContainer = document.getElementById("bio_container");
+        contContainer = document.getElementById("contacts_container");
+        portContainer = document.getElementById("portfolio_container");
+        
+        bioClose = document.getElementById("c_btn_b");
+        contClose = document.getElementById("c_btn_c");
+        portClose = document.getElementById("c_btn_p");
+        
+        bioContainer.addEventListener('scroll', function(e) {limitScroll(e, bioClose);});
+        
+        contContainer.addEventListener('scroll', function(e) {limitScroll(e, contClose);});
+        
+        portContainer.addEventListener('scroll', function(e) {limitScroll(e, portClose);});
+    }
 };
 
 function openPortfolio(){
     portContainer.style.display = 'block';
-    portContainer.style.transform = 'translateY('+ (-(document.body.clientHeight)) +'px)';
-    
+    catchMovement = setInterval(function() {portContainer.style.transform = 'translateY('+ (-(Math.min(document.documentElement.clientHeight, window.innerHeight || 0))) +'px)'}, 50);
     portClose.style.display = 'block';
 }
 
 function closePortfolio(){
-    portContainer.style.transform = 'translateY('+ ((document.body.clientHeight)) +'px)';
-    
+    portContainer.style.transform = 'translateY('+ (Math.min(document.documentElement.clientHeight, window.innerHeight || 0)) +'px)';
     portClose.style.display = 'none';
+    clearInterval(catchMovement);
     setTimeout(function() {portContainer.style.display = 'none' }, 400);
 }
 
@@ -79,6 +83,22 @@ function closeContacts(){
 function contResize(container, tr){
     if(container.style.display == 'block')
         container.style.transform = tr;
+}
+
+function revealPhone(o){
+
+    n = document.getElementById('phone');
+
+    v = 'JzM1MSAnICsgJycgKyAnODk2ICcgKyAnNzgzNCc=';
+
+    console.log(n);
+
+    n.innerHTML = n.innerHTML + eval(atob(v));
+    n.href = 'tel:+39'+ eval(atob(v)).replaceAll(' ', '');
+
+    //hack orribile
+    document.getElementById('phone-1').remove();
+    document.getElementById('phone-2').remove();
 }
 
 window.onresize = () => {
